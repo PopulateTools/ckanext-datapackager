72a8e1e5aac18f56f11a9aeef252604607bb28a4
Alberto Miedes <albertomg994@gmail.com>
2019-01-28 18:32:41 +0100

    Support updating datapackages

--- ckanext/datapackager/logic/action/create.py
+++ ckanext/datapackager/logic/action/create.py
@@ -110,7 +110,11 @@ def _package_create_with_unique_name(context, dataset_dict, name=None):
         dataset_dict['name'] = name
 
     try:
-        res = toolkit.get_action('package_create')(context, dataset_dict)
+        try:
+            toolkit.get_action('package_show')(context, { 'id': dataset_dict['name'] })
+            print toolkit.get_action('package_update')(context, dataset_dict)
+        except toolkit.ObjectNotFound as e:
+            print toolkit.get_action('package_create')(context, dataset_dict)
     except toolkit.ValidationError as e:
         if not name and \
            'That URL is already in use.' in e.error_dict.get('name', []):
